c85d94ceb8b7140eb6eafeb5f5f2d50a33f2610d
Powkachu <pauline.kobersi@gmail.com>
2015-10-21 15:33:45 +0200

    Fix birthday

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
   	@users_today = User.where("extract(month from birthday) = ? AND extract(day from birthday) = ?", Date.today.strftime('%m'), Date.today.strftime('%d'))
   	@users_incoming = User.all.select do | user |
-  		(user.birthday.yday - Date.today.yday) < 7 && (user.birthday.yday - Date.today.yday) >= 1
+  		user.birthday && (user.birthday.yday - Date.today.yday) < 7 && (user.birthday.yday - Date.today.yday) >= 1
   	end
   end
 
