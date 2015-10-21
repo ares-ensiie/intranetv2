5653ac01773d3d305cf6d9c82708a785e576974f
Powkachu <pauline.kobersi@gmail.com>
2015-10-21 15:22:15 +0200

    Ajout des anniversaires sur l'intranet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
 
   def index
+  	@users_today = User.where("extract(month from birthday) = ? AND extract(day from birthday) = ?", Date.today.strftime('%m'), Date.today.strftime('%d'))
+  	@users_incoming = User.all.select do | user |
+  		(user.birthday.yday - Date.today.yday) < 7 && (user.birthday.yday - Date.today.yday) >= 1
+  	end
   end
 
   def coming_soon
