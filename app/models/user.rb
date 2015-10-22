8a7f66cbed98bf62f537d2c5de6e38a8fedef8dc
Powkachu <pauline.kobersi@gmail.com>
2015-10-22 17:23:03 +0200

    Ajout avatar profil public et maj style edit profil

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@ class User < ActiveRecord::Base
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
-  }
+  },
+  default_url: ActionController::Base.helpers.image_path("unknown_user.png")
 
   has_attached_file :cv
   # Validate the attached image is image/jpg, image/png, etc
