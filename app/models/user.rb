48afd407857838bc73d61f1c6398cf911a21919a
Hurter Jonathan <jonathan@hurter.fr>
2015-10-24 16:52:16 +0200

    Fix : L'image unknown_user ne saffichait pas

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
     square: '200x200#',
     medium: '300x300>'
   },
-  default_url: ActionController::Base.helpers.image_path("unknown_user.png")
+    default_url: "/unknown_user.png"
 
   has_attached_file :cv
   # Validate the attached image is image/jpg, image/png, etc
