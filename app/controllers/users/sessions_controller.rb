8686d9919de2434dc58878f33a9791eb1d22c488
johnsudaar <jonathan.hurter@laposte.net>
2015-11-06 22:31:32 +0100

    Redirection après le login !

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,9 +3,12 @@ class Users::SessionsController < Devise::SessionsController
   # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    if params[:redirect_to].present?
+      store_location_for(resource, params[:redirect_to])
+    end
+    super
+  end
 
   # POST /resource/sign_in
   # def create
