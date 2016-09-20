dd6ce7269558e6d0906d26e8db14b5079b18756e
Thibaut Buchert <thibaut.buchert@gmail.com>
2016-09-20 18:21:22 +0200

    Nettoyage vues

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,7 +1,10 @@
 class CalendarController < ApplicationController
 
   def index
-    $cur_prom= current_user.promo
+    @cur_prom = current_user.promo
+    @calendars = APP_CONFIG['calendar']['list']
+    @key = APP_CONFIG['calendar']['key']
+    @default = APP_CONFIG['calendar']['default']
   end
 
 end
