362758562bb30417be9259008cf21131f29c10ec
Obiwan1995 <brice.miclo@gmail.com>
2016-10-21 16:31:45 +0200

    Ajout du "remember_me" sur l'intranet
    L'utilisateur reste automatiquement connecté pendant deux semaines s'il coche la case
    S'il ne coche pas la case, c'est comme avant

--- config/initializers/ldap_authenticatable.rb
+++ config/initializers/ldap_authenticatable.rb
@@ -21,6 +21,9 @@ module Devise
               user = User.find_or_create_by(uid: results[0][:uid][0])
               user.update({uid: results[0][:uid][0], password: "dummydummy", promo: results[0][:roomnumber][0], lastname: results[0][:sn][0], name: results[0][:givenname][0], phone: results[0][:mobile][0], address: results[0][:postalAddress][0], email: results[0][:mail][0]})
               user.save!
+              if params[:user][:remember_me].present?
+                remember_me(user)
+              end
               success!(user)
             else
               fail(:invalid_login)
