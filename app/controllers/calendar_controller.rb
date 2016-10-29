af3e0bbdff76e41d3cc43b854270e023e15d28b9
Thibaut Buchert <thibaut.buchert@gmail.com>
2016-10-29 13:22:58 +0200

    Réorganisation fichier conf
    Les groupes ont été séparés de la liste des calendriers pour rendre le
    code du contrôleur plus simple.
    Les groupes sont désormais dans un OrderedDict pour s'assurer de l'ordre
    des calendriers à l'affichage.

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,19 +1,9 @@
 class CalendarController < ApplicationController
 
   def index
-    # La configuration est commune à toute l'application
-    # il faut la copier avant de la modifier pour ne pas
-    # pas affecter les autres utilisateurs
-    @calendars = APP_CONFIG['calendar']['list'].deep_dup
     @key = APP_CONFIG['calendar']['key']
-    
-    default_cals = APP_CONFIG['calendar']['default']
-    [default_cals['all'], default_cals[current_user.promo]].compact.each { |elt|
-        elt.each { |group_key, group|
-            group.each { |cal|
-                @calendars[group_key][cal]['default'] = 'on'
-            }
-        }
-    }
+    @groups = APP_CONFIG['calendar']['groups']
+    @calendars = APP_CONFIG['calendar']['list']
+    @default_cals = APP_CONFIG['calendar']['default'].values_at('all', current_user.promo).flatten
   end
 end
